0061e84f86b3c88acd7ca5790f353dea712aba90
Leonardo Custodio <leonardo@enjin.io>
2023-07-21 14:23:43 -0300

    Change RequestAccount flow (#23)

--- tests/Feature/GraphQL/Queries/RequestAccountTest.php
+++ tests/Feature/GraphQL/Queries/RequestAccountTest.php
@@ -20,89 +20,29 @@ class RequestAccountTest extends TestCaseGraphQL
 
     public function test_can_request_an_address(): void
     {
-        $response = $this->graphql($this->method, [
-            'callback' => $this->callback,
-        ]);
+        $response = $this->graphql($this->method);
 
-        $this->assertNotEmpty($qrCode = $response['qrCode']);
+        $link = sprintf('https://chart.googleapis.com/chart?chs=512x512&cht=qr&chl=%s', config('enjin-platform.deep_links.proof'));
+        $encodedString = base64_decode(str_replace($link, '', $qrCode = $response['qrCode']));
+        $verificationCode = explode(':', $encodedString)[1];
+
+        $this->assertNotEmpty($qrCode);
         $this->assertNotEmpty($verificationId = $response['verificationId']);
         $this->assertDatabaseHas('verifications', [
-            'code' => explode(':', base64_decode(explode(':', $qrCode)[3]))[1],
+            'code' => $verificationCode,
             'verification_id' => $verificationId,
             'public_key' => null,
         ]);
     }
 
-    public function test_callback_is_embedded_in_qr_code(): void
-    {
-        $response = $this->graphql($this->method, [
-            'callback' => $this->callback,
-        ]);
-
-        $this->assertNotEmpty($qrCode = $response['qrCode']);
-        $this->assertTrue(
-            base64_decode(explode(':', $qrCode)[4]) === $this->callback
-        );
-    }
-
     public function test_is_valid_wallet_deep_link(): void
     {
-        $response = $this->graphql($this->method, [
-            'callback' => $this->callback,
-        ]);
+        $response = $this->graphql($this->method);
 
         $this->assertNotEmpty($qrCode = $response['qrCode']);
         $this->assertStringContainsString(
             config('enjin-platform.deep_links.proof'),
             $qrCode
-        );
-    }
-
-    public function test_it_will_fail_with_no_callback(): void
-    {
-        $response = $this->graphql($this->method, [], true);
-
-        $this->assertStringContainsString(
-            'Variable "$callback" of required type "String!" was not provided.',
-            $response['error'],
-        );
-    }
-
-    // Exception Path
-
-    public function test_it_will_fail_with_null_callback(): void
-    {
-        $response = $this->graphql($this->method, [
-            'callback' => null,
-        ], true);
-
-        $this->assertStringContainsString(
-            'Variable "$callback" of non-null type "String!" must not be null.',
-            $response['error'],
-        );
-    }
-
-    public function test_it_will_fail_with_empty_callback(): void
-    {
-        $response = $this->graphql($this->method, [
-            'callback' => '',
-        ], true);
-
-        $this->assertArraySubset(
-            ['callback' => ['The callback field must have a value.']],
-            $response['error'],
-        );
-    }
-
-    public function test_it_will_fail_with_invalid_callback(): void
-    {
-        $response = $this->graphql($this->method, [
-            'callback' => 'not_a_valid_url',
-        ], true);
-
-        $this->assertArraySubset(
-            ['callback' => ['The callback field must be a valid URL.']],
-            $response['error'],
         );
     }
 }
