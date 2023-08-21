ba859e940ceb0d3d289c078d6df84b393bc9ce0f
Leonardo Custodio <leonardo@custodio.me>
2023-08-21 14:32:25 -0300

    add

---
+++ src/Constants/Substrate/Deposit.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Enjin\Platform\Constants\Substrate;
+
+class Deposit {
+
+    public const COLLECTION_CREATION = '25_000_000_000_000_000_000';
+    public const TOKEN_ACCOUNT_CREATION = '10_000_000_000_000_000';
+    public const ATTRIBUTE_DEPOSIT_BASE = '200_000_000_000_000_000';
+    public const ATTRIBUTE_DEPOSIT_PER_BYTE = '100_000_000_000_000';
+
+}a
