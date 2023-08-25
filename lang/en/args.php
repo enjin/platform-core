fbcae444ec015aebed2d83f0e632cb6dda072ea6
Leonardo Custodio <leonardo@custodio.me>
2023-08-25 11:55:34 -0300

    Add translations

--- lang/en/args.php
+++ lang/en/args.php
@@ -4,4 +4,5 @@ return [
     'common.tokenId' => 'The token ID.',
     'idempotencyKey' => 'The idempotency key to set. It is recommended to use a UUID for this.',
     'tokenId' => 'The token ID to set. This must be unique for this collection.',
+    'simulate' => 'Simulates a transaction without broadcasting it to the network.',
 ];
