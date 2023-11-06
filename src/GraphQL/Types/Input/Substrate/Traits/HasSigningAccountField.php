6905d98014fe6b18c7468dd81615ab6dd2389f77
Leonardo Custodio <leonardo@custodio.me>
2023-11-06 14:23:37 -0300

    add return type

--- src/GraphQL/Types/Input/Substrate/Traits/HasSigningAccountField.php
+++ src/GraphQL/Types/Input/Substrate/Traits/HasSigningAccountField.php
@@ -5,6 +5,7 @@ namespace Enjin\Platform\GraphQL\Types\Input\Substrate\Traits;
 use Enjin\Platform\Rules\ValidSubstrateAccount;
 use Enjin\Platform\Support\Account;
 use Facades\Enjin\Platform\Services\Database\WalletService;
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 use Rebing\GraphQL\Support\Facades\GraphQL;
 
@@ -30,7 +31,7 @@ trait HasSigningAccountField
     /**
      * Get the signing account.
      */
-    public function getSigningAccount(array $args)
+    public function getSigningAccount(array $args): ?Model
     {
         if (!empty($signing = Arr::get($args, 'signingAccount'))) {
             return WalletService::firstOrStore([
@@ -38,6 +39,6 @@ trait HasSigningAccountField
             ]);
         }
 
-
+        return null;
     }
 }
