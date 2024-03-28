cf8c2315aca60136f484ab118512544334f0e93a
Abner Tudtud <114082473+enjinabner@users.noreply.github.com>
2024-03-29 00:36:38 +0800

    [PLA-1700] Add API generic rate limit (#144)

---
+++ tests/Feature/GraphQL/Middleware/RateLimit.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Enjin\Platform\Tests\Feature\GraphQL\Mutations;
+
+use Enjin\Platform\Models\Collection;
+use Enjin\Platform\Tests\Feature\GraphQL\TestCaseGraphQL;
+use Enjin\Platform\Tests\Feature\GraphQL\Traits\HasHttp;
+use Illuminate\Support\Arr;
+
+class RateLimit extends TestCaseGraphQL
+{
+    use HasHttp;
+
+    public function test_it_can_rate_limit(): void
+    {
+        config()->set('enjin-platform.rate_limit.attempts', 1);
+        Collection::factory()->create();
+        $this->json(
+            'POST',
+            '/graphql',
+            ['query' => static::$queries['GetCollections']],
+        );
+        $response = $this->json(
+            'POST',
+            '/graphql',
+            ['query' => static::$queries['GetCollections']],
+        );
+        $result = $response->getData(true);
+        $this->assertStringContainsString('Too Many Attempts.', Arr::get($result, 'message'));
+    }
+
+    public function test_it_will_not_rate_limit(): void
+    {
+        config()->set('enjin-platform.rate_limit.attempts', 1);
+        config()->set('enjin-platform.rate_limit.enabled', false);
+        Collection::factory()->create();
+        $response = $this->json(
+            'POST',
+            '/graphql',
+            ['query' => static::$queries['GetCollections']],
+        );
+        $result = $response->getData(true);
+        $this->assertNotEmpty(Arr::get($result, 'data.GetCollections.edges'));
+        $response = $this->json(
+            'POST',
+            '/graphql',
+            ['query' => static::$queries['GetCollections']],
+        );
+        $result = $response->getData(true);
+        $this->assertNotEmpty(Arr::get($result, 'data.GetCollections.edges'));
+    }
+}
