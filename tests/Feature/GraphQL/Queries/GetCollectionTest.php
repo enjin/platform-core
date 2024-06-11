26979628807cf1ccb88e7806b02dac80f20356be
Leonardo Custodio <leonardo@custodio.me>
2024-06-11 16:42:27 -0300

    Fixes tests

--- tests/Feature/GraphQL/Queries/GetCollectionTest.php
+++ tests/Feature/GraphQL/Queries/GetCollectionTest.php
@@ -88,8 +88,8 @@ class GetCollectionTest extends TestCaseGraphQL
             ],
             'attributes' => [
                 [
-                    'key' => $this->collectionAttribute->key,
-                    'value' => $this->collectionAttribute->value,
+                    'key' => Hex::safeConvertToString($this->collectionAttribute->key),
+                    'value' => Hex::safeConvertToString($this->collectionAttribute->value),
                 ],
             ],
             'tokens' => [
