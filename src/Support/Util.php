05e35850d2ec76eedd0119195522c688cb67f3cc
Leonardo Custodio <leonardo@custodio.me>
2024-06-28 12:12:01 -0300

    improve

--- src/Support/Util.php
+++ src/Support/Util.php
@@ -7,13 +7,13 @@ class Util
     /**
      * Create a JSON-RPC encoded string.
      */
-    public static function createJsonRpc(string $method, array $params = []): string
+    public static function createJsonRpc(string $method, array $params = [], ?int $id = null): string
     {
         return json_encode([
             'jsonrpc' => '2.0',
             'method' => $method,
             'params' => $params,
-            'id' => random_int(1, 999999999),
+            'id' => $id ?? random_int(1, 999999999),
         ], JSON_THROW_ON_ERROR);
     }
 
