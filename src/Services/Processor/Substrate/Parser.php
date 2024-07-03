a02d0806f6ef8d6964f538c9bc8571a5c5e2e068
Leonardo Custodio <leonardo@enjin.io>
2024-07-03 11:25:34 -0300

    [PLA-1873] Make sync work with both matrix versions (#193)

--- src/Services/Processor/Substrate/Parser.php
+++ src/Services/Processor/Substrate/Parser.php
@@ -417,6 +417,10 @@ class Parser
 
         foreach ($data as [$key, $attribute]) {
             $attributeKey = $this->serializationService->decode('attributeStorageKey', $key);
+            // TODO: We dont use but we could decode the storage of an attribute to get
+            //          value: Bytes
+            //          deposit: Compact<u128>
+            //          depositor: Option<AccountId>
             $attributeData = $this->serializationService->decode('bytes', $attribute);
 
             $collection = $this->getCachedCollection(
@@ -565,11 +569,7 @@ class Parser
             }
         }
 
-        if ($hotSync) {
-            CollectionRoyaltyCurrency::upsert($insertData, uniqueBy: ['collection_id', 'currency_collection_chain_id', 'currency_token_chain_id']);
-        } else {
-            CollectionRoyaltyCurrency::insert($insertData);
-        }
+        CollectionRoyaltyCurrency::upsert($insertData, uniqueBy: ['collection_id', 'currency_collection_chain_id', 'currency_token_chain_id']);
     }
 
     /**
