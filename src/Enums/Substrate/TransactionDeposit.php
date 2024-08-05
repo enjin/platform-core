9247b0d29f68e8f17952c9d931d65463b840ca36
Leonardo Custodio <leonardo@enjin.io>
2024-08-05 11:40:53 -0300

    [PLA-1927] Adds deposits for Marketplace and FuelTanks (#219)

--- src/Enums/Substrate/TransactionDeposit.php
+++ src/Enums/Substrate/TransactionDeposit.php
@@ -13,6 +13,8 @@ enum TransactionDeposit: string
     case TOKEN_ACCOUNT = '10000000000000000';
     case ATTRIBUTE_BASE = '50000000000000000';
     case ATTRIBUTE_PER_BYTE = '25000000000000';
+    case LISTING = '507225000000000000';
+    case FUEL_TANK = '2500000000000000000';
 
     public function toGMP(): GMP
     {
@@ -26,6 +28,8 @@ enum TransactionDeposit: string
             self::TOKEN_ACCOUNT => gmp_init(self::TOKEN_ACCOUNT->value),
             self::ATTRIBUTE_BASE => gmp_init(self::ATTRIBUTE_BASE->value),
             self::ATTRIBUTE_PER_BYTE => gmp_init(self::ATTRIBUTE_PER_BYTE->value),
+            self::LISTING => gmp_init(self::LISTING->value),
+            self::FUEL_TANK => gmp_init(self::FUEL_TANK->value),
         };
     }
 }
