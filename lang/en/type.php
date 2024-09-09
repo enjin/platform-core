9e2190d7b25c9d717c81404da535cbd93143d349
Leonardo Custodio <leonardo@custodio.me>
2024-09-09 15:53:07 -0300

    lint

--- lang/en/type.php
+++ lang/en/type.php
@@ -61,7 +61,6 @@ return [
     'event_param.description' => 'An event param.',
     'event_param.field.type' => 'The value type of the param.',
     'event_param.field.value' => 'The value of the param.',
-    'integer_range.description' => "A string value that can be used to represent a range of integer numbers.  Use a double full stop to supply a range between 2 integers.  For example an integer range that looks like this: \n\n\"3..8\"\n\nWill be automatically expanded to:\n\n[3, 4, 5, 6, 7, 8]",
     'integer_ranges_array.description' => "An array that can be used to represent ranges of integer numbers.  Use a double full stop to supply a range between 2 integers in the array.  For example an integer ranges array that looks like this: \n\n[\"1\", \"3..8\", \"11\", \"15..18\"]\n\nWill be automatically expanded to:\n\n[1, 3, 4, 5, 6, 7, 8, 11, 15, 16, 17, 18]",
     'json.description' => 'A type that represents json data.',
     'page_info.field.endCursor' => 'The next cursor.',
