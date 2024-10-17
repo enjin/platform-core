5e1971717ee40e95b6ad322dd8462c65573db42e
Abner Tudtud <abner@enjin.io>
2024-10-17 19:19:23 +0800

    Improve query

---
+++ database/migrations/2024_10_17_061240_add_managed_to_transactions_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Enjin\Platform\Support\Account;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+return new class () extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->boolean('managed')->default(false)->index()->after('state');
+
+            DB::statement("
+                UPDATE transactions SET managed = 1
+                WHERE wallet_public_key IS  NULL
+                or wallet_public_key IN('".implode("','", Account::managedPublicKeys())."')
+            ");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('transactions', function (Blueprint $table) {
+            $table->dropColumn('managed');
+        });
+    }
+};
