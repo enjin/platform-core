bbcfa14f4ffaaa9276a97e5029225dfcd0021330
Etay Matzliah <etaymatzliah@gmail.com>
2024-12-05 19:42:38 +0200

    Improved CreateToken args + some other

--- lang/en/args.php
+++ lang/en/args.php
@@ -2,7 +2,7 @@
 
 return [
     'common.tokenId' => 'The token ID.',
-    'idempotencyKey' => 'The idempotency key to set. It is recommended to use a UUID for this.',
-    'tokenId' => 'The token ID to set. This must be unique for this collection.',
+    'idempotencyKey' => 'A unique key used to prevent duplicate operations. If multiple mutations are sent with the same key, only one will be broadcasted. Using a UUID is recommended.',
+    'tokenId' => 'The token ID to create. This must be unique for this collection.',
     'simulate' => 'Simulates a transaction without broadcasting it to the network.',
 ];
--- lang/en/enum.php
+++ lang/en/enum.php
@@ -4,13 +4,24 @@ return [
     'crypto_signature.description' => 'The type of encryption algorithm used to sign messages.',
     'event_type.description' => 'The event types related to blockchain token transactions.',
     'filter_type.description' => 'The type of filter to use when chaining queries.',
-    'freeze_state_type.description' => 'The freeze state type.',
+    'freeze_state_type.description' => <<<MD
+Defines the token's freeze state, determining if and how it can be frozen. Options include:
+- **PERMANENT**: The token will be permanently frozen, and the freeze state cannot be changed.
+- **TEMPORARY**: The token will be temporarily frozen and can become transferable again if [thawed](https://docs.enjin.io/docs/freezing-thawing#thawing-an-entire-collection).
+- **NEVER**: The token cannot be frozen, ensuring it remains transferable at all times.
+MD,
     'freezable_type.description' => 'The freezable objects supported on-chain.',
     'model_type.description' => 'The model type.',
     'network_type.description' => 'The network type.',
     'pallet_identifier.description' => 'The on-chain pallet identifier.',
     'token_market_behavior_type.description' => 'The market behavior types a token supports.',
-    'token_mint_cap_type.description' => 'The mint cap. The token will be classified as fungible if the cap is set to either infinite, or supply with a value higher than one.',
+    'token_mint_cap_type.description' => <<<MD
+Configures the supply type for the token. Options include:
+- **SUPPLY**: A fixed supply model where the cap amount sets the maximum tokens that can exist in circulation. Burned tokens can be re-minted.
+- **COLLAPSING_SUPPLY**: A dynamic supply model where the cap amount defines the total tokens that can ever be minted. Burned tokens cannot be re-minted.
+- **SINGLE_MINT (DEPRECATED)**: Allows the token to be minted only once. Functions as a collapsing supply model with the cap amount set to the initial supply.
+- **INFINITE**: Allows unlimited token creation with no supply cap.
+MD,
     'token_type.description' => 'The token type, fungible or non-fungible.',
     'transaction_method.description' => 'The currently supported transactions.',
     'transaction_result.description' => 'The result status of a transaction.',
