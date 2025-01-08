040dd08ce447b7ec7fd0ea0e790cfb7c08303e46
Abner Tudtud <abner@enjin.io>
2025-01-08 09:26:49 +0800

    refactor

--- src/GraphQL/Types/Scalars/Traits/HasIntegerRanges.php
+++ src/GraphQL/Types/Scalars/Traits/HasIntegerRanges.php
@@ -13,12 +13,10 @@ trait HasIntegerRanges
 
     public function expandRanges($values): array
     {
-        $self = new self();
-
         return collect($values)
             ->flatten()
-            ->map(function ($range) use ($self) {
-                if ($self->isIntegerRange($range)) {
+            ->map(function ($range) {
+                if ($this->isIntegerRange($range)) {
                     [$start, $end] = explode('..', $range, 2);
                     $range = [];
                     while ($start <= $end) {
