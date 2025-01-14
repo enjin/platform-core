e9f37f77897a5dd412ca7b00f1f17f2b2aa632a0
Simon Evans <simon@enjin.io>
2025-01-14 12:35:10 +0000

    Add configurable margin to QR images.

--- config/enjin-platform.php
+++ config/enjin-platform.php
@@ -229,6 +229,7 @@ return [
     'qr' => [
         'adapter' => PlatformQrAdapter::class,
         'size' => env('QR_CODE_SIZE', 512),
+        'margin' => env('QR_CODE_MARGIN', 1),
         'format' => env('QR_CODE_FORMAT', 'png'),
         'image' => env('QR_CODE_IMAGE_URL'),
         'image_size' => env('QR_CODE_IMAGE_SIZE', .20),
