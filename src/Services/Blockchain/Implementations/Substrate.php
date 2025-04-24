91b73ca2d4d1dbf7e311864059e74407f6bc9d18
Leonardo Custodio <leonardo@enjin.io>
2025-04-24 11:21:40 +0200

    [PLA-2188] Fixes issue with new OperatorTransferToken (#336)

--- src/Services/Blockchain/Implementations/Substrate.php
+++ src/Services/Blockchain/Implementations/Substrate.php
@@ -339,7 +339,7 @@ class Substrate implements BlockchainServiceInterface
             $this->encodeTokenId($args),
             $args['source'],
             $args['amount'],
-            $args['operatorPaysDeposit'],
+            Arr::get($args, 'operatorPaysDeposit', false),
         ];
 
         return new OperatorTransferParams(...$data);
