bea570c80da297cd2c490fb1a0ca086b9d43792a
Talles Borges <talles.borges92@gmail.com>
2025-10-08 15:18:04 -0300

    Add persistent HTTP connection pooling to SubstrateHttpClient
    - Store PendingRequest client instance to reuse across requests
    - Use shared HandlerStack with CurlHandler for connection pooling
    - Reduces TCP/TLS handshake overhead on subsequent requests

--- src/Clients/Implementations/SubstrateHttpClient.php
+++ src/Clients/Implementations/SubstrateHttpClient.php
@@ -4,12 +4,17 @@ namespace Enjin\Platform\Clients\Implementations;
 
 use Arr;
 use Enjin\Platform\Clients\Abstracts\JsonHttpAbstract;
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
 use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Http\Client\PendingRequest;
 use Illuminate\Http\Client\RequestException;
 
 class SubstrateHttpClient extends JsonHttpAbstract
 {
     protected string $url;
+    protected ?PendingRequest $client = null;
+    protected static ?HandlerStack $sharedHandler = null;
 
     /**
      * Create a new http client instance.
@@ -21,19 +26,29 @@ class SubstrateHttpClient extends JsonHttpAbstract
         $this->url = str_replace('wss', 'https', $host);
     }
 
+    #[\Override]
+    protected function getClient(): PendingRequest
+    {
+        self::$sharedHandler ??= HandlerStack::create(new CurlHandler());
+
+        return $this->client ??= parent::getClient()->withOptions([
+            'handler' => self::$sharedHandler,
+        ]);
+    }
+
     /**
      * @throws ConnectionException
      * @throws RequestException
      */
     public function jsonRpc(string $method, array $params): mixed
     {
-        $result = $this->getClient()->post($this->url, [
+        $response = $this->getClient()->post($this->url, [
             'jsonrpc' => '2.0',
             'method' => $method,
             'params' => $params,
             'id' => mt_rand(1, 999999999),
         ]);
 
-        return Arr::get($this->getResponse($result), 'result');
+        return Arr::get($this->getResponse($response), 'result');
     }
 }
